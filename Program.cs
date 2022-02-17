1af5dfd000e0631de36c3cb0fb02049595df02b5
Navyan23 <97940058+Navyan23@users.noreply.github.com>
2022-02-17 12:25:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloconsoleapp
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
