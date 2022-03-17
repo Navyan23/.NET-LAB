4c7b84f40f0f2e1dffede16af2d5a3381fbf58ee
Navyan23 <97940058+Navyan23@users.noreply.github.com>
2022-03-17 11:31:23 +0530

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Winapplication1
+namespace wnapplication3
 {
     public partial class Form1 : Form
     {
@@ -17,67 +17,48 @@ namespace Winapplication1
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            lbl_words.Text = NumtoWord(long.Parse(txt_num.Text));
+            string inputString, revstr = "";
+            int Length;
+            inputString = txtInput.Text;
+            Length = inputString.Length - 1;
+            while (Length >= 0)
+            {
+                revstr = revstr + inputString[Length];
+                Length--;
+            }
+            MessageBox.Show("Reverse String Is : " + revstr, "Result");
         }
-        public string NumtoWord(long number)
+
+        private void button2_Click(object sender, EventArgs e)
         {
-            string word = "";
-            if (number == 0)
-            {
-                return "Zero";
-            }
-            if (number < 0)
-            {
-                return "Minus" + Math.Abs(number);
-            }
-            if (number / 10000000 > 0)
-            {
-                word += NumtoWord(number / 10000000) + "Corer"; number %= 10000000;
-            }
-        if (number / 100000 > 0)
-            {
-                word += NumtoWord(number / 100000) + "Lacs";
-                number %= 100000;
-            }
-            if (number / 1000 > 0)
-            {
-                word += NumtoWord(number / 1000) + "Thousand";
-                number %= 1000;
-            }
-            if (number / 100 > 0)
-            {
-                word += NumtoWord(number / 100) + "Hundred";
-                number %= 100;
-            }
-            if (number > 0)
-            {
-                string[] units = new string[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "ten","Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-                string[] Tens = new string[] { "Zero", "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-                if (number < 20)
-                {
-                    word += units[number];
-                }
-                else
-                {
-                    word += Tens[number / 10];
-                    if (number % 10 > 0)
-                    {
-                        word += units[number % 10];
-                    }
-                }
-            }
-            return word;
+            string inputString;
+            inputString = txtInput.Text;
+            MessageBox.Show("The String After Trimming : " + inputString.Trim(), "Result");
+
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string inputString;
+            inputString = txtInput.Text;
+            inputString = inputString.PadLeft(10, '*');
+            inputString = inputString.PadRight(15, '*');
+            MessageBox.Show("String After Padding : " + inputString, "Result");
+        }
+
     }
 }
 
-    
 
